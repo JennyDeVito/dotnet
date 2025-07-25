341ca58615415f9ed5924e1928812c0b3d80a17c
Jenny DeVito <jdevito@live.com>
2025-07-25 14:18:56 -0300

    Adicionado projeto com exemplo de código Main

---
+++ ExemploFundamentosMain/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Exemplo de programa com padrão Main
+namespace ExemploFundamentosMain
+{
+    class Program // diz onde é o inicio do programa
+    {
+        
+        static void Main(string[] args) // comun em versões .NET 5 para baixo
+        {
+            // a implementação do código deve ser feita dentro do Main
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
+
+// 
+//Console.WriteLine("Hello World");
